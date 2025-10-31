40457cc5e0f76ef524fb089952c5718c78c491d0
Karan20P <karan24800@gmail.com>
2025-10-31 12:05:26 +0530

    created vite app

---
+++ Book-Vite-App/src/component/Book.jsx
@@ -0,0 +1,12 @@
+const Book = () => {
+  return (
+    <div>
+      <img src="" alt="Book Image" witdth={200} height={200} />
+      <h3>Title : Physics</h3>
+      <h4>Price : ₹400</h4>
+      <br />
+      <button>Add to cart</button>
+    </div>
+  );
+};
+export default Book;
