5888d7caf4d7df1e3e711536392b337aac29738c
Karan20P <karan24800@gmail.com>
2025-11-07 12:56:46 +0530

    made changes

--- Shopping-Vite-App/client/src/App.jsx
+++ Shopping-Vite-App/client/src/App.jsx
@@ -9,6 +9,18 @@ function App() {
 
   return (
    <BrowserRouter>
+   <Routes>
+          <Route path='/' element={<UserDashboard/>}>
+            <Route index element={<ShoppingItems/>}/>
+            <Route path='cart' element={<h1>View Cart</h1>}/>
+            <Route path='order' element={<h1>View Order</h1>}/>
+            <Route path='logout' element={<h1>Logout</h1>}/>
+          </Route>
+          <Route path='/register' element={<h1>Register Here</h1>}/>
+          <Route path='/login' element={<h1>Login Here</h1>}/>
+          <Route path='*' element={<h1>404 Error Page</h1>}/>
+        </Routes>
+
    </BrowserRouter>
   )
 }
