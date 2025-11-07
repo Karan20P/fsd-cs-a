233d27f15144935f511d3a3850f638ba9ce96286
Karan20P <karan24800@gmail.com>
2025-11-07 13:05:55 +0530

    fixed App.jsx

--- Shopping-Vite-App/client/src/App.jsx
+++ Shopping-Vite-App/client/src/App.jsx
@@ -1,28 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import UserDashboard from "./pages/UserDashboard";
+import ShoppingItems from "./components/ShoppingItems";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <BrowserRouter>
-   <Routes>
-          <Route path='/' element={<UserDashboard/>}>
-            <Route index element={<ShoppingItems/>}/>
-            <Route path='cart' element={<h1>View Cart</h1>}/>
-            <Route path='order' element={<h1>View Order</h1>}/>
-            <Route path='logout' element={<h1>Logout</h1>}/>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserDashboard />}>
+            <Route index element={<ShoppingItems />} />
+            <Route path="cart" element={<h1>View Cart</h1>} />
+            <Route path="order" element={<h1>View Order</h1>} />
+            <Route path="logout" element={<h1>Logout</h1>} />
           </Route>
-          <Route path='/register' element={<h1>Register Here</h1>}/>
-          <Route path='/login' element={<h1>Login Here</h1>}/>
-          <Route path='*' element={<h1>404 Error Page</h1>}/>
+          <Route path="/register" element={<h1>Register Here</h1>} />
+          <Route path="/login" element={<h1>Login Here</h1>} />
+          <Route path="*" element={<h1>404 Error Page</h1>} />
         </Routes>
-
-   </BrowserRouter>
-  )
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
